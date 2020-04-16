cb26c1117ad6e512f437924ebff4e7e193726a75
Prem Pratap Singh <pratapprem88@gmail.com>
2020-04-16 15:11:52 +0530

    Inventory Management Junit codes

--- TesterAll.java
+++ TesterAll.java
@@ -1,12 +1,13 @@
-package junit.first;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
 @RunWith(Suite.class)
-@Suite.SuiteClasses({stringPallindromeTesting.class,EmployeeTesting.class,Demo1Testing.class,CalculatorTesting.class})
+@Suite.SuiteClasses({
+	VideoTest.class,
+	VideoStoreTest.class
+})
+
 public class TesterAll {
 	
-
 }
